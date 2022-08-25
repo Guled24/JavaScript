4e0875de202ee40bd692bcafced38cbfd12f16bd
Guled Muse <gmuse12@hotmail.com>
2022-08-25 19:31:09 +0100

    .first attempt.

---
+++ java1.js
@@ -0,0 +1,39 @@
+// First Attempt at Javascript
+console.log("Hello world, my name is Guled.");
+
+// Print number
+console.log("21");
+
+// Print a string
+console.log("I am eager to learn about javascript");
+
+// Create a variable and re-initialise that variable print both variables to show that you did it properly
+let number=22;
+console.log(number)
+number= 23;
+console.log(number);
+
+// Constant variable
+
+const first_name = "Guled";
+
+console.log (first_name);
+
+
+// first_name = "Hamza"; This code will cause an error as the variable const is a static variable. 
+
+
+//Print both boolean types
+let b = ( 1==3 ) 
+console.log (b);
+
+let c = ( 2==2 )
+console.log (c);
+
+// Create an undefines variable and print it off
+let shoe_size;
+console.log (shoe_size);
+
+//Create a null variable and print it off
+let height = null;
+console.log(height);
